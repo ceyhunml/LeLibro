b7b38a8d35ff544761854be085cd43771306635f
ceyhunml <ceyhunmmli@gmail.com>
2025-09-14 23:47:48 +0400

    change

--- LeLibro/MenuViewController.swift
+++ LeLibro/MenuViewController.swift
@@ -6,29 +6,36 @@
 //
 
 import UIKit
+import CoreData
 
 class MenuViewController: UIViewController {
 
     @IBOutlet weak var collection: UICollectionView!
     
+    let manager = CoreDataManager()
+    
+    var books = [BookEntity]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.titleView = makeNavigationLogoView(imageName: "mainLogo", size: 180)
+        books = manager.fetchBooks()
+        print(books)
+        print(books.count)
     }
     
 
 }
 
 
-extension MenuViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-}
+//extension MenuViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        2
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.
+//    }
+//    
+//}
